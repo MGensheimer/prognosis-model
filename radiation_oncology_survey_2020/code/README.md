6c38d9aa388aba71f1801014d16b68727d9cef70
Michael Gensheimer <michael.gensheimer@gmail.com>
2020-05-29 09:40:40 -0700

    readme changes

---
+++ radiation_oncology_survey_2020/code/README.md
@@ -0,0 +1,6 @@
+## Selected files
+
+* [train\_model.py](train_model.py): Train prognostic model
+* [process\_data.py](process_data.py): Functions for parsing clinical data. Used by train_model.py.
+* [apply\_model\_to\_radonc\_survey\_pts.py](apply_model_to_radonc_survey_pts.py): Once model trained, apply it to radiation oncology survey study patients in preparation for analysis.
+* [analysis.r](analysis.r): Statistical analysis of survey data, after model predictions have been generated  
--- radiation_oncology_survey_2020/code/apply_model_to_radonc_survey_pts.py
+++ radiation_oncology_survey_2020/code/apply_model_to_radonc_survey_pts.py
@@ -3,7 +3,7 @@ pt_landmarks_notchang = pt_landmarks.loc[pt_landmarks.patient_id.isin(pts_train_
 landmark_data_notchang = landmark_data[pt_landmarks.patient_id.isin(pts_train_test),:]
 landmark_data_notchang_scaled = standardize_data_and_impute_mean(landmark_data_notchang,feature_means,feature_std,feature_include)
 y_notchang = make_surv_array(pt_landmarks_notchang.fu_days.values,pt_landmarks_notchang.dead.values,breaks)
-model_notchang = train_surv_model(input_x=landmark_data_notchang_scaled, input_y=y_notchang, l2=0.1) #l2=0.1
+model_notchang = train_surv_model(input_x=landmark_data_notchang_scaled, input_y=y_notchang, l2=0.1)
 with open(output_dir+'models/'+model_config['model_name']+'/model_survival_notchang_arch.json', "w") as text_file:
     text_file.write("%s" % model_notchang.to_json())
 
@@ -34,10 +34,6 @@ np.save(output_dir+'models/'+model_config['model_name']+'/chang_model_predsurv',
 temp = pd.DataFrame(pred_surv)
 temp.columns=[str(i) for i in model_config['breaks'][1:]]
 temp.to_parquet(output_dir+'models/'+model_config['model_name']+'/chang_model_predsurv.parquet')
-#eval_dates.to_feather(output_dir+'models/'+model_config['model_name']+'/chang_model_results.feather')
-#store_export = pd.HDFStore(output_dir+'models/'+model_config['model_name']+'/chang_model_predsurv.h5')
-#store_export.append('pred_surv', temp)
-#store_export.close()
 
 eval_dates_2wkshift=chang_pts.loc[:,['patient_id','MRN_FULL','tx_date']]
 eval_dates_2wkshift.rename(columns={'tx_date':'eval_date'},inplace=True)
@@ -55,11 +51,9 @@ for i in range(len(eval_dates)):
 
 n_feats_report=5
 sign_dict={True:' +',False:' -'}
-#sign_dict={True:' -',False:' +'} 
 eval_dates['improve_surv_feats'] = ''
 eval_dates['worsen_surv_feats'] = ''
-result=landmark_data_chang_scaled*np.transpose(np.tile(weights[0],landmark_data_chang_scaled.shape[0])) #neg because this model has reversed coef sign compared to other ones
-#result=landmark_data_chang_scaled*np.transpose(np.tile(-weights[0],landmark_data_chang_scaled.shape[0])) #neg because this model has reversed coef sign compared to other ones
+result=landmark_data_chang_scaled*np.transpose(np.tile(weights[0],landmark_data_chang_scaled.shape[0]))
 sorted_feats=np.argsort(result,axis=1)
 for i in range(len(eval_dates)):
 	improve_surv_feats=feature_names.feature_name.iloc[sorted_feats[i,:-(n_feats_report+1):-1]]
@@ -70,17 +64,3 @@ for i in range(len(eval_dates)):
 	eval_dates['worsen_surv_feats'].iloc[i]="".join(i + j for i, j in zip([sign_dict[x] for x in worsen_surv_positive_sign],worsen_surv_feats))
 
 eval_dates.to_parquet(output_dir+'models/'+model_config['model_name']+'/chang_model_results.parquet')
-
-#Chang pts earliest medonc visit
-chang_pts = pd.read_excel(data_dir+'misc/chang_outcomes_w_mrn0s.xlsx', dtype={'mrn_full':str})
-chang_pts.rename(columns={'mrn_full':'MRN_FULL'},inplace=True)
-chang_pts = pd.merge(chang_pts,pt_data['pts_scirdb'].loc[:,['patient_id','MRN_FULL']],on='MRN_FULL')
-chang_pts.drop_duplicates(subset=['patient_id'],inplace=True)
-pts_chang=pts_all.loc[pts_all.isin(chang_pts.patient_id)]
-medonc_dept_names=['GI ONCOLOGY','THORACIC ONCOLOGY','URO ONCOLOGY','BREAST ONCOLOGY','GYN ONCOLOGY','CCSB INFUSION TREATMENT CENTER','HEAD/NECK ONCOLOGY BW','ENDOCRINE CLINIC','CUTANEOUS ONCOLOGY','SARCOMA','SARCOMA BW']
-medonc_notes=pt_data['notes'].loc[pt_data['notes'].department_name.isin(medonc_dept_names)]
-earliest_medonc_note=medonc_notes.sort_values(by=['pat_enc_contact_date']).groupby('patient_id').pat_enc_contact_date.first()
-earliest_medonc_note.rename('earliest_medonc_note',inplace=True)
-earliest_medonc_note=earliest_medonc_note.loc[earliest_medonc_note.index.isin(pts_chang)]
-temp=pd.merge(chang_pts,earliest_medonc_note,on='patient_id').loc[:,['MRN_FULL','mrn_full_no0','patient_id','earliest_medonc_note']]
-temp.to_parquet(output_dir+'models/'+model_config['model_name']+'/chang_earliest_medonc.parquet')
